282b1c338b3ca9a83e01a61ee098e0874d8e08f7
Chaturasan Ramineni <98098897+ChaturasanR@users.noreply.github.com>
2026-07-27 20:26:58 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    char_map = dict({"}": "{", ")" : "(", "]": "["})
+
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        for ch in s:
+            if ch in self.char_map.values():
+                stack.append(ch)
+                continue
+            
+            if ch in self.char_map.keys():
+                if not stack or stack.pop() != self.char_map[ch]:
+                    return False
+                
+        return len(stack) == 0
+
+        
